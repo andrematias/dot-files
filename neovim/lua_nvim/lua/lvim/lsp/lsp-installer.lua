0ebf06fd0de573c14f39e0dc2385e225bfbdd6f1
Andre Matias <dev.andrematias@gmail.com>
2022-02-18 13:42:16 -0300

    Add lsp lua server settings

--- neovim/lua_nvim/lua/lvim/lsp/lsp-installer.lua
+++ neovim/lua_nvim/lua/lvim/lsp/lsp-installer.lua
@@ -9,6 +9,11 @@ lsp_installer.on_server_ready(function(server)
     on_attach = require("lvim.lsp.handlers").on_attach,
   }
 
+  if server.name == "sumneko_lua" then
+    local sumneko_opts = require("lvim.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
   -- Set options to the server
   server:setup(opts)
 end)
