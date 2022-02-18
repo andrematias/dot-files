9da16b6904d72ef6b7e21d6301787f1e55b1e710
Andre Matias <dev.andrematias@gmail.com>
2022-02-17 23:46:08 -0300

    Add keymaps neovim in lua

---
+++ neovim/lua_nvim/lua/lvim/keymaps.lua
@@ -0,0 +1,54 @@
+-- local term_opts = { silent = true }
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+keymap("", "<Space>", "<Nop>", opts)
+
+-- Normal mode
+-- To windows navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+
+
+-- Resize with arrow keys
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+
+-- Close current buffer 
+keymap("n", "<leader>c", ":bdelete<CR>", opts)
+
+
+-- Insert mode
+-- Option to esc with jk or kj
+keymap("i", "kj", "<Esc>", opts)
+keymap("i", "jk", "<Esc>", opts)
+
+
+-- Visual mode
+-- Indent mode in Visual mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+
+-- Move selected lines
+keymap("v", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("v", "K", ":move '<-2<CR>gv-gv", opts)
+
+
+-- Visual Block mode
+-- Move selected lines
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+
